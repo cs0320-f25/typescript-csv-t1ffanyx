914979aca4d2458daf891eca2143990b96f8a8f5
t1ffanyx <tiffany_xiao@brown.edu>
2025-09-12 21:17:39 -0400

    extended test suite: missing data, fields, special characters, s, schema

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -1,12 +1,15 @@
 import { parseCSV } from "../src/basic-parser";
 import * as path from "path";
+import { z } from "zod";
 
 const PEOPLE_CSV_PATH = path.join(__dirname, "../data/people.csv");
+
+// given tests (result: arrays)
 
 test("parseCSV yields arrays", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   
-  expect(results).toHaveLength(5);
+  expect(results).toHaveLength(12);
   expect(results[0]).toEqual(["name", "age"]);
   expect(results[1]).toEqual(["Alice", "23"]);
   expect(results[2]).toEqual(["Bob", "thirty"]); // why does this work? :(
@@ -16,7 +19,85 @@ test("parseCSV yields arrays", async () => {
 
 test("parseCSV yields only arrays", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
+
   for(const row of results) {
     expect(Array.isArray(row)).toBe(true);
   }
 });
+
+
+/**
+ * extended test suite:
+ * - missing data
+ * - attributes of a fields: commas, spaces, quotations 
+ * - special characters
+ * - schema
+ * - column headers [come back to test]
+ * - large files [come back to test]
+ */
+
+// missing values handled
+
+test("parseCSV missing values", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH)
+
+  expect(results[5]).toEqual(["", "23"]);
+});
+
+// attributes of fields: quotes, commas, spaces
+
+test("parseCSV: quoted fields", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH);
+  expect(results[6]).toEqual(["Alice", "fifty"]);
+});
+
+test("parseCSV: commas", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH);
+  expect(results[7]).toEqual(["Milton, Dan", "29"]);
+});
+
+test("parseCSV: spaces", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH);
+  expect(results[8]).toEqual(["Ali Bally", "20"]);
+});
+
+test("parseCSV: decimal", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH);
+  expect(results[11]).toEqual(["Bella", "12.9"]);
+});
+
+
+// special characters
+
+test("parseCSV: special character last", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH);
+  expect(results[9]).toEqual(["MICHA!!!", "10"]);
+});
+
+test("parseCSV: special character first", async () => {
+  const results = await parseCSV(PEOPLE_CSV_PATH);
+  expect(results[10]).toEqual(["~nina", "16"]);
+});
+
+// schema: not specified (returns strings, in first example)
+
+// schema doesn't match provided
+
+test("parseCSV: schema provided", async () => {
+  const PersonSchema = z.object({
+    name: z.string(),
+    age: z.union([z.string(), z.number()]),
+  });
+
+  const results = await parseCSV(PEOPLE_CSV_PATH, { schema: PersonSchema });
+
+  expect(results[1]).toEqual([{ name: "Alice", age: "23"}]);
+});
+
+// column header
+
+
+
+
+
+
